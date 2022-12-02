658f97ff0f4d5a59900ed1a41e5878aa234a8535
tesing032 <tesing032@gmail.com>
2022-12-02 18:34:50 +0530

    sec commit

---
+++ deploy.php
@@ -0,0 +1,11 @@
+ <?php
+    # webhook for project deployment
+    if ($_GET['token'] === 'secret_key') {
+        $cmd = shell_exec(" git clone https://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@github.com/tesing032/new-repo.git ");
+        echo $cmd;
+        echo 'Deployment sucess';
+    } else {
+        echo 'Error';
+    }
+
+    ?>
